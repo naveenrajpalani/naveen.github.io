818e56a81df01d3b73789226f93faf352766bb24
naveenrajpalani <154815201+naveenrajpalani@users.noreply.github.com>
2024-02-04 11:53:08 +0000

    Sun, Feb 4, 2024, 5:23 PM +05:30

--- PROJECT_NAME/PROJECT_NAME/settings.py
+++ PROJECT_NAME/PROJECT_NAME/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "myapp",
+    "APP_NAME",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
